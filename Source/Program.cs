f6035f81abad7b270d8a7c57aca6c60085adecdc
Corey Prophitt <prophitt.corey@gmail.com>
2013-01-11 23:08:48 -0500

    Refactoring

---
+++ Source/Program.cs
@@ -0,0 +1,64 @@
+﻿//
+// File: Program.cs
+//
+// Author: Corey Prophitt <prophitt.corey@gmail.com>
+//
+// Copyright: See the readme.
+//
+
+
+using System;
+using System.Linq;
+using System.Xml;
+
+
+namespace Emailer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Parse the config file
+			XmlDocument config = new XmlDocument();
+			try
+			{
+				config.Load("../../config.xml");
+			}
+			catch(Exception ex) 
+			{
+				Console.WriteLine("ERROR: Exception while reading configuration file.", Color.Red);
+				Console.WriteLine(ex.Message, Color.Red);
+			}
+
+			var root = config.FirstChild;
+
+			var username = root["username"].InnerXml;
+			var password = root["password"].InnerXml;
+			var subject = root["subject"].InnerXml;
+			var body = root["body"].InnerXml;
+			var host = root["host"].InnerXml;
+			var recipients = root["recipients"].InnerXml.Trim();
+
+			// Parse the recipients file
+			Console.WriteLine("INFO: Reading " + recipients, Color.Yellow);
+
+			var emails = new CsvReader(recipients).Read();
+			if (emails != null)
+			{
+				Console.WriteLine(string.Format("INFO: Attempting to send {0} emails.", emails.Count), Color.Cyan);
+
+				// Email all recipients a copy of the message
+				var massMailer = new MassMailer(username, subject, body, host, password);
+				massMailer.Add(emails);
+				massMailer.Send();
+			}
+			else
+			{
+				Console.WriteLine("ERROR: No emails extracted.", Color.Red);
+			}
+
+			Console.WriteLine("Finished! Press enter to quit.", Color.Cyan);
+			System.Console.ReadLine();
+		}
+	}
+}
---
+++ Source/SmtpMailer.cs
@@ -0,0 +1,99 @@
+﻿//
+// File: SmtpMailer.cs
+//
+// Author: Corey Prophitt <prophitt.corey@gmail.com>
+//
+// Copyright: See the readme.
+//
+
+#region Using Statements
+
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+
+#endregion
+
+namespace Emailer
+{
+	class SmtpMailer
+    {
+
+        #region Properties
+
+        public string host { get; set; }
+		public string from { get; set; }
+		public string to { get; set; }
+		public string subject { get; set; }
+		public string body { get; set; }
+		public int port { get; set; }
+
+        #endregion
+
+        #region Creation & Destruction
+
+        public SmtpMailer( int port = 587 )
+		{
+			this.port = port;
+		}
+
+		public SmtpMailer( string host, int port = 587 )
+		{
+			this.host = host;
+			this.port = port;
+		}
+
+		public SmtpMailer( string from, string to, string subject, string body, int port = 587 )
+		{
+			this.from = from;
+			this.to = to;
+			this.subject = subject;
+			this.body = body;
+			this.port = port;
+		}
+
+		public SmtpMailer( string host, string from, string to, string subject, string body, int port = 587 )
+		{
+			this.host = host;
+			this.from = from;
+			this.to = to;
+			this.subject = subject;
+			this.body = body;
+			this.port = port;
+		}
+
+        #endregion
+
+        #region Methods
+
+        public void Send( string password, uint delay = 0 )
+		{
+			var email = new MailMessage(from, to, subject, body);
+
+			var smtp = new SmtpClient(host, port);
+			smtp.Credentials = new NetworkCredential(from, password);
+			smtp.EnableSsl = true;
+
+			try
+			{
+				if (delay > 0)
+				{
+					Thread.Sleep(new TimeSpan(delay));
+				}
+				smtp.Send(email);
+				//smtp.SendAsync(email, null); 
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error: an exception was caught!", Color.Red);
+				Console.WriteLine(ex.Message, Color.Red);
+			}
+
+        }
+
+        #endregion
+
+    } // end class SmtpMailer
+} // end namespace
